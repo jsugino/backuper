5012f3697b616fac1e7cc6332dd6cbb17f866568
Junsei Sugino <junsei.sugino@gmail.com>
2018-11-24 16:25:50 +0900

    initial version

---
+++ src/main/java/mylib/backuper/Backuper.java
@@ -0,0 +1,43 @@
+package mylib.backuper;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Backuper
+{
+  public Path rootFolder;
+
+  public static void main( String arg[] )
+  {
+    if ( arg.length < 3 ) { usage(); return; }
+
+    try {
+      DataBase db = new DataBase(Paths.get(arg[0]));
+
+      db.readDB(arg[1]);
+      db.storageMap.get(arg[1]).dump(System.out);
+      db.scanFolder(arg[1]);
+      db.storageMap.get(arg[1]).dump(System.out);
+      db.writeDB(arg[1]);
+
+      db.readDB(arg[2]);
+      db.storageMap.get(arg[2]).dump(System.out);
+      db.scanFolder(arg[2]);
+      db.storageMap.get(arg[2]).dump(System.out);
+      db.writeDB(arg[2]);
+
+      Backuper from = new Backuper();
+      from.rootFolder = Paths.get(arg[0]);
+    } catch ( Exception ex ) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static void usage()
+  {
+    System.err.println("usage : java -jar file.jar dbName fromDir toDir");
+    System.err.println("    dbName  : specify database file name");
+    System.err.println("    fromDir : source folder name");
+    System.err.println("    toDir   : destination folder name");
+  }
+}
---
+++ src/main/java/mylib/backuper/DataBase.java
@@ -0,0 +1,251 @@
+package mylib.backuper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class DataBase
+{
+  public static SimpleDateFormat STDFORMAT = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+
+  // ======================================================================
+  public static class Storage
+  {
+    Path rootFolder;
+    HashMap<Path,Folder> folders = null;
+
+    public String toString()
+    {
+      StringBuffer buf = new StringBuffer(rootFolder.toString());
+      if ( folders != null ) {
+	buf.append(" (");
+	int cnt = 0;
+	for ( Folder folder : folders.values() ) {
+	  cnt += folder.files.size();
+	}
+	buf.append(cnt);
+	buf.append(" files)");
+      }
+      return buf.toString();
+    }
+
+    public void dump( PrintStream out )
+    {
+      for ( Map.Entry<Path,Folder> folder : folders.entrySet() ) {
+	out.println(folder.getKey());
+	for ( Map.Entry<String,File> fileent : folder.getValue().files.entrySet() ) {
+	  File file = fileent.getValue();
+	  out.println(file.hashValue+'\t'+STDFORMAT.format(new Date(file.lastModified))+'\t'+file.length+'\t'+fileent.getKey());
+	}
+      }
+    }
+  }
+
+  public static class Folder
+  {
+    public HashMap<String,File> files = new HashMap<>();
+  }
+
+  public static class File
+  {
+    public String hashValue;
+    public long lastModified;
+    public long length;
+  }
+
+  // ======================================================================
+  public final static String CONFIGNAME = "folders.conf";
+
+  public Path dbFolder;
+  public HashMap<String,Storage> storageMap;
+  public MessageDigest digest;
+
+  public DataBase( Path dbFolder )
+  throws IOException
+  {
+    this.dbFolder = dbFolder;
+    this.storageMap = new HashMap<>();
+    try {
+      digest = MessageDigest.getInstance("MD5");
+    } catch ( NoSuchAlgorithmException ex ) {
+      throw new IOException("Cannot initialize 'digest MD5'",ex);
+    }
+    LinkedList<String> list = new LinkedList<>();
+    try (
+      Stream<String> stream = Files.lines(dbFolder.resolve(CONFIGNAME))
+    ) { stream.forEach(list::add); }
+    for ( String line : list ) {
+      if ( line.length() == 0 || line.startsWith("#") ) return;
+      int idx = line.indexOf('=');
+      if ( idx <= 0 ) {
+	System.err.println("No '=' : "+line);
+	return;
+      }
+      String key = line.substring(0,idx).trim();
+      Path path = Paths.get(line.substring(idx+1).trim());
+      if ( !path.isAbsolute() ) {
+	System.err.println("Not Absolute Path : "+line);
+	return;
+      }
+      Storage storage = new Storage();
+      storage.rootFolder = path;
+      storageMap.put(key,storage);
+    }
+  }
+
+  public void readDB( String storageName )
+  throws IOException
+  {
+    Storage storage = storageMap.get(storageName);
+    if ( storage == null ) {
+      throw new IOException("Illegal Storage Name : "+storageName);
+    }
+
+    LinkedList<String> list = new LinkedList<>();
+    try (
+      Stream<String> stream = Files.lines(dbFolder.resolve(storageName+".db"))
+    ) { stream.forEach(list::add); }
+    storage.folders = new HashMap<Path,Folder>();
+    Folder folder = null;
+    for ( String line : list ) {
+      int i1;
+      if ( (i1 = line.indexOf('\t')) < 0 ) {
+	folder = storage.folders.get(line);
+	if ( folder == null ) {
+	  folder = new Folder();
+	  storage.folders.put(Paths.get(line),folder);
+	}
+      } else {
+	int i2 = line.indexOf('\t',i1+1);
+	int i3 = line.indexOf('\t',i2+1);
+	File file = new File();
+	file.hashValue = line.substring(0,i1);
+	try {
+	  file.lastModified = STDFORMAT.parse(line.substring(i1+1,i2)).getTime();
+	} catch ( ParseException ex ) {
+	  System.err.println("Cannot parse time : "+line);
+	  file.lastModified = 0L;
+	}
+	file.length = Long.parseLong(line.substring(i2+1,i3));
+	folder.files.put(line.substring(i3+1),file);
+      }
+    }
+  }
+
+  public void scanFolder( String storageName )
+  throws IOException
+  {
+    Storage storage = storageMap.get(storageName);
+    if ( storage == null ) {
+      throw new IOException("Illegal Storage Name : "+storageName);
+    }
+
+    HashMap<Path,Folder> origFolders = storage.folders;
+    storage.folders = new HashMap<Path,Folder>();
+    LinkedList<Path> list = new LinkedList<>();
+    try (
+      Stream<Path> stream = Files.walk(storage.rootFolder)
+    ) { stream.filter(x -> !Files.isDirectory(x)).forEach(list::add); }
+    for ( Path path : list ) {
+      Path reltiv = storage.rootFolder.relativize(path);
+      Path parent = reltiv.getParent();
+      String name = reltiv.getFileName().toString();
+      Folder folder = null;
+      File origfile = null;
+      File newfile = new File();
+      if ( (folder = storage.folders.get(parent)) == null ) {
+	folder = new Folder();
+	storage.folders.put(parent,folder);
+      }
+      if ( folder.files.get(name) != null ) {
+	System.err.println("duplicate : "+path);
+	return;
+      }
+      folder.files.put(name,newfile);
+      newfile.length = Files.size(path);
+      newfile.lastModified = Files.getLastModifiedTime(path).toMillis();
+      if ( origFolders != null &&
+	   (folder = origFolders.get(parent)) != null &&
+	   (origfile = folder.files.get(name)) != null &&
+	   newfile.length == origfile.length &&
+	   newfile.lastModified == origfile.lastModified
+      ) {
+	newfile.hashValue = origfile.hashValue;
+      } else {
+	newfile.hashValue = getMD5(path);
+	System.err.println("calculate MD5 "+path);
+      }
+    }
+  }
+
+  public void writeDB( String storageName )
+  throws IOException
+  {
+    Storage storage = storageMap.get(storageName);
+    if ( storage == null ) {
+      throw new IOException("Illegal Storage Name : "+storageName);
+    }
+
+    try ( PrintStream out = new PrintStream(Files.newOutputStream(dbFolder.resolve(storageName+".db")))
+    ) { storage.dump(out); }
+  }
+
+  // --------------------------------------------------
+  public String getMD5( Path path )
+  throws IOException
+  {
+    digest.reset();
+    try ( InputStream in = Files.newInputStream(path) )
+    {
+      byte buf[] = new byte[1024*64];
+      int len;
+      while ( (len = in.read(buf)) > 0 ) {
+	digest.update(buf,0,len);
+      }
+    }
+    String str = Base64.getEncoder().encodeToString(digest.digest());
+    int idx = str.indexOf('=');
+    if ( idx > 0 ) str = str.substring(0,idx);
+    return str;
+  }
+
+  // --------------------------------------------------
+  /*
+  public void update( String storageName )
+  throws IOException
+  {
+    if ( !Files.isDirectory(rootFolder) ) {
+      throw new IllegalArgumentException("Not a folder "+rootFolder);
+    }
+
+    Folder folder = storageMap.get(storageName);
+    if ( folder == null ) {
+      folder = new Folder();
+      folder.name = storageName;
+      storageMap.put(storageName,folder);
+    }
+
+    try ( Stream<Path> stream = Files.walk(rootFolder) )
+    { stream
+	.filter(x -> !Files.isDirectory(x))
+	.map(rootFolder::relativize)
+	.forEach(System.out::println);
+    }
+  }
+  */
+}
